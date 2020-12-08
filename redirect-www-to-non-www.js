4131d4f024e95f668b83e9925c95c09b423623c7
Too Much IO <toomuchio@users.noreply.github.com>
2020-12-08 22:28:03 +1100

    Fairly useless readability changes

--- redirect-www-to-non-www.js
+++ redirect-www-to-non-www.js
@@ -2,17 +2,22 @@
 //Scheme is not exposed by event object unfortunately... So it's set to https.
 
 'use strict';
+
+const WWW_PREFIX = "www.";
+const WWW_PREFIX_LEN = WWW_PREFIX.length;
+const CF_ORIGIN_IGNORE = ".cloudfront.net";
+const FORWARD_SCHEME = "https://";
+
 exports.handler = function handler(event, context, callback) {
 	const request = event.Records[0].cf.request;
 	const host = request.headers.host[0].value;
 
-
-	if (host.startsWith("www.") && !host.endsWith(".cloudfront.net")) {
+	if (host.startsWith(WWW_PREFIX) && !host.endsWith(CF_ORIGIN_IGNORE)) {
 		const response = {
 			headers: {
 				'location': [{
 					key: 'Location',
-					value: "https://" + host.slice(4) + request.uri
+					value: FORWARD_SCHEME + host.slice(WWW_PREFIX_LEN) + request.uri
 				 }]
 			},
 			status: '301',
